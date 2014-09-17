ff25572290c8afa48b41da8c6525d06b7812cea7
brco0199 <bryan.p.connelly@gmail.com>
2014-09-17 12:03:06 -0600

    Added if/else/while to test out extension

--- src/hello.java
+++ src/hello.java
@@ -4,6 +4,20 @@ public class hello {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("How are you?");
+		
+		int x = 5;
+		if(x == 5){
+			System.out.println("Hey");
+		}
+		else{
+			System.out.println("Woah");
+			
+		}
+		
+		while(x == 5){
+			System.out.println("Looping");
+			x++;
+		}
 	}
 }
 
